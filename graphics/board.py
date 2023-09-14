899645cb9c15f636f93e314b19f4a9856d075e57
Destinn <donats2004@gmail.com>
2023-09-14 10:34:28 +0300

    feat: add_move added with validator

--- graphics/board.py
+++ graphics/board.py
@@ -30,6 +30,17 @@ class Board():
         board_str += "    " + "   ".join(chr(ord('A') + i) for i in range(self.COL))
 
         return board_str
+    
+    def add_move(self, input) -> int:
+        try:
+            col = int(ord(input[0]) - ord('@'))
+            row = int(input[1:2])
+        except ValueError:
+            print("Недопустимый ход")
+            return -1
+        if (not 0 < col < (self.COL + 1) or not 0 < row < (self.ROW + 1)):
+            return -1
+        return 0
 
 
 if __name__ == "__main__":
@@ -38,5 +49,8 @@ if __name__ == "__main__":
     # Set some cells to be CROSS or CIRCLE for testing
     board.board[5][5] = Cell.CROSS
     board.board[7][7] = Cell.CIRCLE
+
+    print(board.add_move("A5"))
+    print(board.add_move("!!"))
 
     print(board)
