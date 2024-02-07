84b5b360572f36e347620eb3bbb079cab4f1c81a
Destinn <donats2004@gmail.com>
2024-02-07 14:22:42 +0300

    fix: maybe rust new structure

--- calc_rust/src/lib.rs
+++ calc_rust/src/lib.rs
@@ -1,13 +1,22 @@
 use pyo3::prelude::*;
 
+#[pyclass]
+struct Board {}
+
+#[pyclass]
+enum CellStatus {}
+
 #[pyfunction]
 fn calculate_move(given: Board, player_side: CellStatus) -> PyResult<()> {
+    let mut depth = given.get_valid_moves()
     Ok(())
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn calc_rust(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Board>()?;
+    m.add("CellStatus", pyo3::types::PyType::new::<CellStatus>())?;
     m.add_function(wrap_pyfunction!(calculate_move, m)?)?;
     Ok(())
 }
